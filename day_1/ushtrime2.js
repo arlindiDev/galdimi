273354e7ab519d22c34059af4f8c98013165dbd6
TEAM-DEPISCH\galdim.hajredinaj <galdimhajredinaj@gmail.com>
2023-05-13 22:16:52 +0200

    formatted code

--- day_1/ushtrime2.js
+++ day_1/ushtrime2.js
@@ -1,18 +1,16 @@
 document.getElementById("proccess").addEventListener("click", okButtonClicked);
 
 function okButtonClicked() {
-	var number1 = document.getElementById("input").value;
-    var number2 = document.getElementById("input").value;
+        var number1 = document.getElementById("input").value;
+        var number2 = document.getElementById("input").value;
 
-       if (number1 < number2) {
-        console.log("numri 2 eshte ma i madh se numri 1");
+        if (number1 < number2) {
+                console.log("numri 2 eshte ma i madh se numri 1");
 
-} else if (number1 > number2) {
-        console.log("numri 1 eshte ma i madh se numri 1 ")
-        
-} else if (number1 == number2) {
-        console.log("numrat jane te barabarta")
-    }
+        } else if (number1 > number2) {
+                console.log("numri 1 eshte ma i madh se numri 2 ")
 
-
+        } else if (number1 == number2) {
+                console.log("numrat jane te barabarta")
+        }
 }
--- day_1/ushtrime3.js
+++ day_1/ushtrime3.js
@@ -1,15 +1,13 @@
 
 
 var semafori = document.getElementById("input").value;
-var semafori = prompt(" Enter name:")
 
-
-   if (semafori == 1) {
+if (semafori == 1) {
     console.log("Ndal");
 
 } else if (semafori == 2) {
     console.log("behu gati!");
-    
+
 } else if (semafori == 3) {
     console.log("Nisu");
 } else {
