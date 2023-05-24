de6267534ba64ef51904c84f9cf948a148525cd6
Arlind <arlind.hajredinaj@gmail.com>
2023-05-24 18:35:48 +0200

    day 5 lecture

---
+++ day_5/day5.js
@@ -0,0 +1,12 @@
+
+var names = ["Galdim", "Arlind", "Cima", "Majasili", "google", "Hajdari", "hashimi"];
+
+for(var i = 0; i < names.length; i++) {
+
+	var name = names[i].toUpperCase();
+
+	console.log(name[2]);
+}
+
+var name = "Hajdar Veseli";
+console.log(name.toUpperCase());
