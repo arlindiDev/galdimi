b735ba3420519b364ae4b20a4a901d2d56915542
Arlind <arlind.hajredinaj@gmail.com>
2023-05-25 18:27:10 +0200

    day 6 example nested for loops

---
+++ day_6/day6.js
@@ -0,0 +1,20 @@
+// 2 2
+// 2 2
+
+// 3 3 3
+// 3 3 3 
+// 3 3 3
+
+function execute() {
+	var size = document.getElementById('input').value;
+
+	for ( var k = 0; k < size; k ++) {
+		var row = "";
+
+		for(var i = 0; i < size; i++) {
+			row = row + " " + size;
+		}
+
+		console.log(row)
+	}
+}
