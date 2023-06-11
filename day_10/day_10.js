e6a367a229eeba6c07dacf582c99f0992bdc6f16
TEAM-DEPISCH\galdim.hajredinaj <galdimhajredinaj@gmail.com>
2023-06-11 20:01:38 +0200

    me metoden indexOf

--- day_10/day_10.js
+++ day_10/day_10.js
@@ -1,21 +1,66 @@
- function halili (base, exponent) {
+ /* function halili (base, exponent) {
     let result = 1;
     for (let count = 0; count < exponent; count++) {
-      result *= base;
+      result += base;
     }
     return result;
   };
   
-  console.log(halili(2, 10));
+  console.log(halili(2, 10)); */
 
-  // 1- 1*2 -> count = 0
-  // 2*2 = 4-> count = 1
-  // 4*2 8 -> count = 2
-  // 8*2 16 -> count = 3
-  // 16*2 -> count = 4
-  // 5 - 32*2 -> count = 5
-  // 64*2 -> count = 6
-  // 128*2 -> count = 7
-  // 256*2 -> count = 8
-  // 512*2 -> count = 9
-  // 1024 -> count = 10
+ 
+
+ 
+
+  
+
+
+  const power = (base, exponent) => {
+    let result = 1;
+    for (let i= 0; i < exponent; i++) {
+      result += base;
+    }
+    return result;
+  };
+  console.log(power(2, 10));
+
+// 
+
+  const square1 = (x) => { return x * x; };
+const square2 = x => x * x;
+
+console.log(square1(2));
+console.log(square2(2));
+//
+
+
+function minus(a, b) {
+  if (b === undefined) return -a;
+  else return a - b;
+}
+
+console.log(minus(10, 0));
+// → -10
+console.log(minus(10, 5));
+
+
+function wrapValue(n) {
+  let local = n;
+  return () => local;
+}
+
+let wrap1 = wrapValue(1);
+let wrap2 = wrapValue(2);
+console.log(wrap1());
+// → 1
+console.log(wrap2());
+// → 2
+
+
+function multiplier(factor) {
+  return number => number * factor;
+}
+
+let twice = multiplier(2);
+console.log(twice(5));
+// → 10
--- day_9/libri.js
+++ day_9/libri.js
@@ -21,14 +21,14 @@ class Biblioteka {
 
     addLibri(libri) {
 
-        if(this.librat.length == 0 ) {
+        if (this.librat.length == 0) {
             this.librat.push(libri)
             return true
         }
 
 
         // nese libri veq osht ne array mos e shto perndryshe shtoje
-        for (var i = 0; i < this.librat.length; i++) {  
+        for (var i = 0; i < this.librat.length; i++) {
             if (this.librat[i].krahasoLibrat(libri)) {
                 return false
             }
@@ -38,18 +38,35 @@ class Biblioteka {
         return true
         // i = 0 | 0 < 0 
     }
+
+    gjejIndex(libri) {
+        for (var i = 0; i < this.librat.length; i++) {
+            if (this.librat[i].krahasoLibrat(libri)) {
+               return this.librat.indexOf(libri)
+            }
+            //     
+        
+        
+        }
+        return -1
+    }
 }
 
-libri1 = new Libri('Gjenerali', 1234)
-libri2 = new Libri('Hajdari', 1235)
-libri3 = new Libri('Javascript Book', 1235)
+
+
+libri1 = new Libri('Gjenerali', 1234) // object e tipit libri
+libri2 = new Libri('Hajdari', 1235)  // object e tipit libri
+libri3 = new Libri('Javascript Book', 1236)
 libri1.krahasoLibrat(libri2)
 bibloteka1 = new Biblioteka('Naim Frasheri')
 
 bibloteka1.addLibri(libri1)
 bibloteka1.addLibri(libri2)
-bibloteka1.addLibri(libri1)
 bibloteka1.addLibri(libri3)
 
-console.log(bibloteka1.librat)
 
+
+console.log(bibloteka1.librat)
+console.log(bibloteka1.gjejIndex(libri1))
+console.log(bibloteka1.gjejIndex(libri2))
+console.log(bibloteka1.gjejIndex(libri3))
